1862b4c899d3f125f63bd2b6261f53079a9b4eca
Henry <hz@therealhenry.com>
2014-12-25 21:42:25 -0800

    I WILL FUCKING LEARN ROUTES UGJ

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -10,15 +10,22 @@
           className (aget event "target" "className")]
           (session/setter className value)))
 
-(defn fireBasePlayground []
+(defn new-post-view []
   [:div
     [:input.hostel_name     {:type "text" :placeholder "Hostel Name"     :on-change #(handle-input-update %) }]
     [:input.job_description {:type "text" :placeholder "Job description" :on-change #(handle-input-update %)}]
     [:input.location        {:type "text" :placeholder "Location"        :on-change #(handle-input-update %)}]
     [:input.email           {:type "text" :placeholder "Email"           :on-change #(handle-input-update %)}]
     [:input.website         {:type "text" :placeholder "website"         :on-change #(handle-input-update %)}]
-   ]
-  )
+
+    [:a.routes {:on-click #(println "go home")} "home"]
+  ])
+
+(defn home-view []
+  [:div.home
+   [:h1 "This is the home page"]
+   [:p "Welcome to the p tag of the home page."]
+   [:a.routes {:on-click #(println "go make new post")} "post a job"]])
 
 (defn submit []
   (let [fb (js/Firebase. "https://jobs-board.firebaseio.com/job-listings")]
@@ -26,9 +33,12 @@
     )
   )
 
+
+
 (defn app-view []
    [:div
-    [fireBasePlayground]
+    [new-post-view]
+    [home-view]
     [submit]])
 
 (reagent/render-component [app-view] (.getElementById js/document "app"))
