3f867149a7d7876fe8d362ead2eeccb8bfb37c79
Henry <hz@therealhenry.com>
2014-12-25 21:53:00 -0800

    refactored core

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -5,12 +5,14 @@
             [main.post :as post]
             [ajax.core :as ajax]))
 
-(defn handle-input-update [event]
-    (let [value     (aget event "target" "value")
-          className (aget event "target" "className")]
-          (session/setter className value)))
+
 
 (defn new-post-view []
+  (defn handle-input-update [event]
+    (let [value     (aget event "target" "value")
+          className (aget event "target" "className")]
+      (session/setter className value)))
+
   [:div
     [:input.hostel_name     {:type "text" :placeholder "Hostel Name"     :on-change #(handle-input-update %) }]
     [:input.job_description {:type "text" :placeholder "Job description" :on-change #(handle-input-update %)}]
@@ -18,28 +20,25 @@
     [:input.email           {:type "text" :placeholder "Email"           :on-change #(handle-input-update %)}]
     [:input.website         {:type "text" :placeholder "website"         :on-change #(handle-input-update %)}]
 
-    [:a.routes {:on-click #(println "go home")} "home"]
+    (let [fb (js/Firebase. "https://jobs-board.firebaseio.com/job-listings")]
+      [:a {:href "#" :on-click #(session/post2fb fb)} "submit"])
+
+
+    [:a.routes {:on-click #(println "go home")} "HOME PAGE"]
   ])
 
 (defn home-view []
   [:div.home
-   [:h1 "This is the home page"]
-   [:p "Welcome to the p tag of the home page."]
-   [:a.routes {:on-click #(println "go make new post")} "post a job"]])
+   [:h1 "WELCOME TO THE JOBS BOARD"]
+   [:a.routes {:on-click #(println "go make new post")} "POST A NEW JOB"]])
 
-(defn submit []
-  (let [fb (js/Firebase. "https://jobs-board.firebaseio.com/job-listings")]
-    [:a {:href "#" :on-click #(session/post2fb fb)} "submit"]
-    )
-  )
 
 
 
 (defn app-view []
    [:div
     [new-post-view]
-    [home-view]
-    [submit]])
+    [home-view]])
 
 (reagent/render-component [app-view] (.getElementById js/document "app"))
 
