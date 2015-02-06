94a503cc01a96e411d9e4c82bf4f80db22980d1a
hzhu <hz@therealhenry.com>
2015-02-05 20:52:12 -0800

    added uri regex

--- src/main/helper-functions.cljs
+++ src/main/helper-functions.cljs
@@ -7,6 +7,13 @@
 (defn make-date [epoch]
   (subs (.toDateString (js/Date. epoch)) 4 10))
 
+;; Given string, returns nil or URL.
+(defn regex-url? [string] 
+  (let [regex #"^[a-zA-Z0-9\-\.]+\.(com|org|me|io|net|co|edu|uk|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es)$"
+        value (str/lower-case string)
+        nil-or-url (re-matches regex value)]
+        (nil-or-url 0)))
+
 ;; Given value, returns a valid email or nil
 (defn regex-email? [value]
   (re-matches #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" value))
