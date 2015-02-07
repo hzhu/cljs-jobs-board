31d767ab49311ddef67be52b31dfb64662383308
hzhu <hz@therealhenry.com>
2015-02-07 15:13:06 -0800

    transforming all contenteditable's url and emails to <a>

--- src/main/helper-functions.cljs
+++ src/main/helper-functions.cljs
@@ -7,22 +7,26 @@
 (defn make-date [epoch]
   (subs (.toDateString (js/Date. epoch)) 4 10))
 
-;; Given string, returns nil or URL.
+;; Given string, returns nil or valid URL.
 (defn regex-url? [string] 
   (let [regex #"^[a-zA-Z0-9\-\.]+\.(com|org|me|io|net|co|edu|uk|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es)$"
         value (str/lower-case string)
         nil-or-url (re-matches regex value)]
-        (nil-or-url 0)))
+        (if (nil? nil-or-url)
+          (println "this isn't a valid url")
+          (str (nil-or-url 0)))))
 
-;; Given value, returns a valid email or nil
+;; Given value, returns nil or valid email.
 (defn regex-email? [value]
-  (re-matches #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" value))
+  (re-matches #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" value))     
        
 ;; If string is email, returns an anchor tag.
 (defn email-to-html [string]
-    (if (nil? (regex-email? string))
-      (str string)
-      (str "<a href=\"mailto:" string "\">" string "</a>")))
+  (cond
+    (= string (regex-email? string)) (str "<a href=\"mailto:" string "\">" string "</a>")
+    (= string (regex-url? string))   (str "<a href=" string ">" string "</a>")
+    (nil? (regex-url? string)) (str string)
+    (nil? (regex-email? string)) (str string)))
 
 ;; Returns original string with HTML transformed emails
 (defn transform-email [string]
